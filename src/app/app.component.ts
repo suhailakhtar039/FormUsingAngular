c1b8939dac82c6b589baadcf8e7e9193faac3f8c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-03-26 16:33:00 +0530

    Starting Form REACTIVE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,48 +6,5 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
-  defaultQuestion = "pet";
-  answer = ''
-  genders = ['male', 'female']
-  user={
-    username:'',
-    email:'',
-    secretQuestion:'',
-    answer:'',
-    gender:''
-  };
-  submitted = false;
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signupForm.setValue({
-    //   userData:{
-    //     username:suggestedName,
-    //     email:''
-    //   },
-    //   secret:'pet',
-    //   questionAnswer:'',
-    //   gender:'male'
-    // })
-    this.signupForm.form.patchValue({
-      userData:{
-        username:suggestedName
-      }
-    })
-  }
-
-  // onSubmit(form: NgForm){
-  //   console.log(form)
-  // }
-
-  onSubmit() {
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-    this.submitted = true;
-    this.signupForm.reset();
-  }
+  genders = ['male', 'female'];
 }
