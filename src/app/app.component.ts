6e7a5ebec258579bdba6f2f9bba8e4bb9529c953
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-03-26 16:01:44 +0530

    template driven form completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,30 @@ export class AppComponent {
   defaultQuestion = "pet";
   answer = ''
   genders = ['male', 'female']
+  user={
+    username:'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    this.signupForm.setValue({
+    // this.signupForm.setValue({
+    //   userData:{
+    //     username:suggestedName,
+    //     email:''
+    //   },
+    //   secret:'pet',
+    //   questionAnswer:'',
+    //   gender:'male'
+    // })
+    this.signupForm.form.patchValue({
       userData:{
-        username:suggestedName,
-        email:''
-      },
-      secret:'pet',
-      questionAnswer:'',
-      gender:'male'
+        username:suggestedName
+      }
     })
   }
 
@@ -30,6 +43,12 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm)
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
+    this.signupForm.reset();
   }
 }
